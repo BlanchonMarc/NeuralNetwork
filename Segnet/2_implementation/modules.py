4ac20e72484e767f9f77a23d03588db078f0cb31
BlanchonMarc <fr.marc.blanchon@gmail.com>
2018-02-12 14:23:33 +0100

    Custom Classes for layers with adaptative size

--- Segnet/2_implementation/modules.py
+++ Segnet/2_implementation/modules.py
@@ -4,6 +4,70 @@ import torch.nn.functional as F
 import torch.nn as nn
 
 
+class SegNetBlockUp(nn.Module):
+    def __init__(self, in_size, out_size, layer_size):
+
+        super(SegNetBlockUp, self).__init__()
+        if layer_size == 2:
+            self.unpool = nn.MaxUnpool2d(2, 2)
+            self.conv1 = conv2DBatchNormRelu(in_size, in_size, 3, 1, 1)
+            self.conv2 = conv2DBatchNormRelu(in_size, out_size, 3, 1, 1)
+        else:
+            self.unpool = nn.MaxUnpool2d(2, 2)
+            self.conv1 = conv2DBatchNormRelu(in_size, in_size, 3, 1, 1)
+            self.conv2 = conv2DBatchNormRelu(in_size, in_size, 3, 1, 1)
+            self.conv3 = conv2DBatchNormRelu(in_size, out_size, 3, 1, 1)
+
+    def forward(self, inputs, indices, output_shape, layer_size):
+
+        if layer_size == 2:
+            outputs = self.unpool(input=inputs, indices=indices,
+                                  output_size=output_shape)
+            outputs = self.conv1(outputs)
+            outputs = self.conv2(outputs)
+
+        else:
+            outputs = self.unpool(input=inputs, indices=indices,
+                                  output_size=output_shape)
+            outputs = self.conv1(outputs)
+            outputs = self.conv2(outputs)
+            outputs = self.conv3(outputs)
+
+        return outputs
+
+
+class SegNetBlockDown(nn.Module):
+    def __init__(self, in_size, out_size, layer_size):
+
+        super(SegNetBlockDown, self).__init__()
+
+        if layer_size == 2:
+            self.conv1 = conv2DBatchNormRelu(in_size, out_size, 3, 1, 1)
+            self.conv2 = conv2DBatchNormRelu(out_size, out_size, 3, 1, 1)
+            self.maxpool_with_argmax = nn.MaxPool2d(2, 2, return_indices=True)
+
+        else:
+            self.conv1 = conv2DBatchNormRelu(in_size, out_size, 3, 1, 1)
+            self.conv2 = conv2DBatchNormRelu(out_size, out_size, 3, 1, 1)
+            self.conv3 = conv2DBatchNormRelu(out_size, out_size, 3, 1, 1)
+            self.maxpool_with_argmax = nn.MaxPool2d(2, 2, return_indices=True)
+
+    def forward(self, inputs, layer_size):
+
+        if layer_size == 2:
+            outputs = self.conv1(inputs)
+            outputs = self.conv2(outputs)
+            unpooled_shape = outputs.size()
+            outputs, indices = self.maxpool_with_argmax(outputs)
+        else:
+            outputs = self.conv1(inputs)
+            outputs = self.conv2(outputs)
+            outputs = self.conv3(outputs)
+            unpooled_shape = outputs.size()
+            outputs, indices = self.maxpool_with_argmax(outputs)
+        return outputs, indices, unpooled_shape
+
+
 class conv2DBatchNormRelu(nn.Module):
     def __init__(self, in_channels, n_filters, k_size, stride, padding,
                 bias=True, dilation=1):
@@ -20,68 +84,4 @@ class conv2DBatchNormRelu(nn.Module):
 
     def forward(self, inputs):
         outputs = self.cbr_unit(inputs)
-        return outputs
-
-
-class segnetDown2(nn.Module):
-    def __init__(self, in_size, out_size):
-        super(segnetDown2, self).__init__()
-        self.conv1 = conv2DBatchNormRelu(in_size, out_size, 3, 1, 1)
-        self.conv2 = conv2DBatchNormRelu(out_size, out_size, 3, 1, 1)
-        self.maxpool_with_argmax = nn.MaxPool2d(2, 2, return_indices=True)
-
-    def forward(self, inputs):
-        outputs = self.conv1(inputs)
-        outputs = self.conv2(outputs)
-        unpooled_shape = outputs.size()
-        outputs, indices = self.maxpool_with_argmax(outputs)
-        return outputs, indices, unpooled_shape
-
-
-class segnetDown3(nn.Module):
-    def __init__(self, in_size, out_size):
-        super(segnetDown3, self).__init__()
-        self.conv1 = conv2DBatchNormRelu(in_size, out_size, 3, 1, 1)
-        self.conv2 = conv2DBatchNormRelu(out_size, out_size, 3, 1, 1)
-        self.conv3 = conv2DBatchNormRelu(out_size, out_size, 3, 1, 1)
-        self.maxpool_with_argmax = nn.MaxPool2d(2, 2, return_indices=True)
-
-    def forward(self, inputs):
-        outputs = self.conv1(inputs)
-        outputs = self.conv2(outputs)
-        outputs = self.conv3(outputs)
-        unpooled_shape = outputs.size()
-        outputs, indices = self.maxpool_with_argmax(outputs)
-        return outputs, indices, unpooled_shape
-
-
-class segnetUp2(nn.Module):
-    def __init__(self, in_size, out_size):
-        super(segnetUp2, self).__init__()
-        self.unpool = nn.MaxUnpool2d(2, 2)
-        self.conv1 = conv2DBatchNormRelu(in_size, in_size, 3, 1, 1)
-        self.conv2 = conv2DBatchNormRelu(in_size, out_size, 3, 1, 1)
-
-    def forward(self, inputs, indices, output_shape):
-        outputs = self.unpool(input=inputs, indices=indices,
-                              output_size=output_shape)
-        outputs = self.conv1(outputs)
-        outputs = self.conv2(outputs)
-        return outputs
-
-
-class segnetUp3(nn.Module):
-    def __init__(self, in_size, out_size):
-        super(segnetUp3, self).__init__()
-        self.unpool = nn.MaxUnpool2d(2, 2)
-        self.conv1 = conv2DBatchNormRelu(in_size, in_size, 3, 1, 1)
-        self.conv2 = conv2DBatchNormRelu(in_size, in_size, 3, 1, 1)
-        self.conv3 = conv2DBatchNormRelu(in_size, out_size, 3, 1, 1)
-
-    def forward(self, inputs, indices, output_shape):
-        outputs = self.unpool(input=inputs, indices=indices,
-                              output_size=output_shape)
-        outputs = self.conv1(outputs)
-        outputs = self.conv2(outputs)
-        outputs = self.conv3(outputs)
         return outputs
--- Segnet/2_implementation/segnet.py
+++ Segnet/2_implementation/segnet.py
@@ -12,31 +12,41 @@ class segnet(nn.Module):
     def __init__(self, in_channels, n_classes):
         super(segnet, self).__init__()
 
-        self.layer_1 = segnetDown2(in_channels, 64)
-        self.layer_2 = segnetDown2(64, 128)
-        self.layer_3 = segnetDown3(128, 256)
-        self.layer_4 = segnetDown3(256, 512)
-        self.layer_5 = segnetDown3(512, 512)
+        self.layer_1 = SegNetBlockDown(in_channels, 64, 2)
+        self.layer_2 = SegNetBlockDown(64, 128, 2)
+        self.layer_3 = SegNetBlockDown(128, 256, 3)
+        self.layer_4 = SegNetBlockDown(256, 512, 3)
+        self.layer_5 = SegNetBlockDown(512, 512, 3)
 
-        self.layer_6 = segnetUp3(512, 512)
-        self.layer_7 = segnetUp3(512, 256)
-        self.layer_8 = segnetUp3(256, 128)
-        self.layer_9 = segnetUp2(128, 64)
-        self.layer_10 = segnetUp2(64, n_classes)
+        self.layer_6 = SegNetBlockUp(512, 512, 3)
+        self.layer_7 = SegNetBlockUp(512, 256, 3)
+        self.layer_8 = SegNetBlockUp(256, 128, 3)
+        self.layer_9 = SegNetBlockUp(128, 64, 2)
+        self.layer_10 = SegNetBlockUp(64, n_classes, 2)
 
     def forward(self, inputs):
 
-        down1, indices_1, unpool_shape1 = self.layer_1(inputs)
-        down2, indices_2, unpool_shape2 = self.layer_2(down1)
-        down3, indices_3, unpool_shape3 = self.layer_3(down2)
-        down4, indices_4, unpool_shape4 = self.layer_4(down3)
-        down5, indices_5, unpool_shape5 = self.layer_5(down4)
+        down1, indices_1, unpool_shape1 = self.layer_1(inputs=inputs,
+                                                       layer_size=2)
+        down2, indices_2, unpool_shape2 = self.layer_2(inputs=down1,
+                                                       layer_size=2)
+        down3, indices_3, unpool_shape3 = self.layer_3(inputs=down2,
+                                                       layer_size=3)
+        down4, indices_4, unpool_shape4 = self.layer_4(inputs=down3,
+                                                       layer_size=3)
+        down5, indices_5, unpool_shape5 = self.layer_5(inputs=down4,
+                                                       layer_size=3)
 
-        up5 = self.layer_6(down5, indices_5, unpool_shape5)
-        up4 = self.layer_7(up5, indices_4, unpool_shape4)
-        up3 = self.layer_8(up4, indices_3, unpool_shape3)
-        up2 = self.layer_9(up3, indices_2, unpool_shape2)
-        up1 = self.layer_10(up2, indices_1, unpool_shape1)
+        up5 = self.layer_6(inputs=down5, indices=indices_5,
+                           output_shape=unpool_shape5, layer_size=3)
+        up4 = self.layer_7(inputs=up5, indices=indices_4,
+                           output_shape=unpool_shape4, layer_size=3)
+        up3 = self.layer_8(inputs=up4, indices=indices_3,
+                           output_shape=unpool_shape3, layer_size=3)
+        up2 = self.layer_9(inputs=up3, indices=indices_2,
+                           output_shape=unpool_shape2, layer_size=2)
+        up1 = self.layer_10(inputs=up2, indices=indices_1,
+                            output_shape=unpool_shape1, layer_size=2)
 
         return up1
 
